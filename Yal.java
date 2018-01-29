cf22f36aa6c4735cc6442fca0223d4b144847740
mahr <benjamin.mahr5@etu.univ-lorraine.fr>
2018-01-29 22:54:53 +0100

    debut des test, erreur lors du lancement de mars.jar

--- Yal.java
+++ Yal.java
@@ -1,7 +1,9 @@
 package yal ;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import yal.analyse.AnalyseurLexical;
@@ -21,11 +23,15 @@ public class Yal {
         try {
             AnalyseurSyntaxique analyseur = new AnalyseurSyntaxique(new AnalyseurLexical(new FileReader(fichier)));
             ArbreAbstrait arbre = (ArbreAbstrait) analyseur.parse().value;
-            System.err.println("expression stockée dans l'arbre : " + arbre);
+            System.err.println("expression stockée dans l'arbre : \n" + arbre);
             
-            // à écrire pour yal0
+            
             arbre.verifier() ; 
-            System.out.println(arbre.toMIPS());
+            //System.out.println(arbre.toMIPS());
+            String str;
+            str = arbre.toMIPS();
+            write(fichier.substring(0,fichier.length()-4),str);
+            System.out.println("Compilation valide");
         } 
         catch (FileNotFoundException ex) {
             System.err.println("Fichier " + fichier + " inexistant") ;
@@ -36,6 +42,18 @@ public class Yal {
         catch (Exception ex) {
             Logger.getLogger(Yal.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    
+    public void write(String nom_fichier, String contenu){
+    	try{
+	    	File f=new File(nom_fichier+".mips");
+	    	FileWriter fileW=new FileWriter(f);
+	    	fileW.write(contenu);
+	    	fileW.close();
+    	}catch(Exception e){
+    		System.err.println("Erreur: le fichier n'a pa été crée");
+    	}
     }
 
     public static void main(String[] args) {
