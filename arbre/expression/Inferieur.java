91ae9736eef217ca2fa5834a07d0dff77441c65f
mahr <benjamin.mahr5@etu.univ-lorraine.fr>
2018-01-30 14:19:44 +0100

    test fini yal0 terminé

--- arbre/expression/Inferieur.java
+++ arbre/expression/Inferieur.java
@@ -30,31 +30,21 @@ public class Inferieur extends Comparaison {
 	}
 	
 	public String toMIPS() {
-	    	StringBuilder str = new StringBuilder();
-	    	str.append(super.toMIPS()+"\n #Inferieur \n");
-	    	str.append("Si"+compteurSi+":\n");
-	    	str.append("# a < b \n");
-	    	str.append("sub $v0, $v0, $t8\n");
-	    	str.append("bltz $v0, alors"+compteurSi+"\n"); //branch on greater than zero
-	    	str.append("alors"+compteurSi+":\n");
-	    	//contenu du si
-<<<<<<< HEAD
-	    	str.append("li $v0,0 \n");
-	    	str.append("j fin"+compteurSi+"\n");
-	    	str.append("sinon"+compteurSi+":\n");
-	    	//Contenu du sinon
-	    	str.append("li $v0,1 \n");
-	    	str.append("fin"+compteurSi+":\n");
-=======
-	    	str.append("li $v0,0 \n"); // si supérieur ou égal à 0 renvoie false
-	    	str.append("j finsi"+compteurSi+"\n");
-	    	str.append("sinon"+compteurSi+":\n");
-	    	//Contenu du sinon
-	    	str.append("li $v0,1 \n"); // sinon inférieur à 0 renvoie true
-	    str.append("j finsi"+compteurSi+"\n");
-	    	str.append("finsi"+compteurSi+":\n");
->>>>>>> 3893abf43e925940ea6ebe46c62fe3339b81bef1
-	    	compteurSi++;
+		StringBuilder str = new StringBuilder();
+		str.append(super.toMIPS()+"\n #Inferieur \n");
+		str.append("Si"+compteurSi+":\n");
+		str.append("# a < b \n");
+		str.append("sub $v0, $v0, $t8\n");
+		str.append("bltz $v0, alors"+compteurSi+"\n"); //branch on greater than zero
+		str.append("alors"+compteurSi+":\n");
+		//contenu du si
+		str.append("li $v0,0 \n");
+		str.append("j fin"+compteurSi+"\n");
+		str.append("sinon"+compteurSi+":\n");
+		//Contenu du sinon
+		str.append("li $v0,1 \n");
+		str.append("fin"+compteurSi+":\n");
+		compteurSi++;
 	    	
 	    	return str.toString();
 	}
